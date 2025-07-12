15f499a5de7366b34ad191d97432c8d2d15e8f23
vinothkumar-m18 <vinothkumarm045@gmail.com>
2025-07-12 20:40:06 +0530

    Move site files to repo root for GitHub Pages

--- E-commerce/index.js
+++
@@ -1,6 +0,0 @@
-document.querySelector(".navbar-toggle").addEventListener("click", function(){
-    document.querySelector(".side-navbar").style.left = "0";
-})
-document.querySelector(".side-navbar-close").addEventListener("click", function(){
-    document.querySelector(".side-navbar").style.left = "-60%";
-})
